05e6f93819cbcce29fa74b996c8051c6fb6b68d1
dragon7-fc <dragon7.fc@gmail.com>
2018-08-16 05:19:52 +0800

    feat: add LeetCode practice code

--- algorithm/Merge Two Binary Trees.py
+++ algorithm/Merge Two Binary Trees.py
@@ -22,6 +22,9 @@ Merged tree:
 	 5   4   7
 Note: The merging process must start from the root nodes of both trees.
  """
+
+
+""" Solution1: 100 ms """
 class Solution:
     def mergeTrees(self, t1, t2):
         """
@@ -41,3 +44,23 @@ class Solution:
                 t1.left = self.mergeTrees(t1.left, t2.left)
                 t1.right = self.mergeTrees(t1.right, t2.right)
             return t1
+
+
+""" Solution2: 92 ms """
+class Solution:
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        if t1 is None:
+            return t2
+        if t2 is None:
+            return t1
+
+        t1.val += t2.val;
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
+        
