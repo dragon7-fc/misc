9e46285a3f888fce72f2c78b52f1f7f3a048c438
dragon7-fc <dragon7.fc@gmail.com>
2018-08-25 23:38:04 +0800

    feat: add LeetCode solution

---
+++ algorithm/Search a 2D Matrix.py
@@ -0,0 +1,45 @@
+""" 
+Search a 2D Matrix
+
+Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has the following properties:
+
+Integers in each row are sorted from left to right.
+The first integer of each row is greater than the last integer of the previous row.
+Example 1:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 3
+Output: true
+Example 2:
+
+Input:
+matrix = [
+  [1,   3,  5,  7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 50]
+]
+target = 13
+Output: false
+
+"""
+
+
+""" Solution: 40 ms """
+class Solution:
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        if not matrix:
+            return False
+        for row in matrix:
+            if target in row:
+                return True
+        return False
