8b011bcb3b16ecdaea6f1c6fc9ce662fabf2737b
dragon7-fc <dragon7.fc@gmail.com>
2019-04-05 16:32:30 +0800

    feat: add transpose matrix solution from udacity

--- algorithm/LeetCode/Transpose Matrix.py
+++ algorithm/LeetCode/Transpose Matrix.py
@@ -34,3 +34,12 @@ class Solution:
             for j in range(len(A[0])):
                 AT[j][i] = A[i][j]
         return AT
+
+"""
+Udacity (Python for Data Analysis)
+
+data = ((0, 1, 2), (3, 4, 5), (6, 7, 8), (9, 10, 11))
+
+data_transpose = tuple(zip(*data))
+print(data_transpose)
+ """
